690f5e13a3fb56958024c36882f049ef06ac4cf2
YissellMen06 <161254666+YissellMen06@users.noreply.github.com>
2024-02-25 19:12:52 -0300

    iniciar
    iniciar

---
+++ script.js
@@ -0,0 +1,9 @@
+const fulImgBox = document.getElementById("fulImgBox")
+fulImg = document.getElementById("fulImgBox")
+
+ function openFulImg(){
+    fulImgBox.style.display = "flex"
+ }
+function closeImg{
+    fulImgBox.style.display = "none";
+}
